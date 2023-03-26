46e0ff64fdf453afeb50436e8de23534e202fda4
Yoel Sillam <redsillam@gmail.com>
2023-03-26 20:07:02 +0200

    Ajout-Button-HELLO

--- Vue/Accueil/vueAccueil.php
+++ Vue/Accueil/vueAccueil.php
@@ -17,5 +17,6 @@
   <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
+      <button>HELLO</button>
   </button>
 </div>
